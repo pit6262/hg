507447eeed325abf186d5b1132d93c742cbec811
Pit <orel-1993@yandex.com>
2020-02-08 09:59:35 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -248,6 +248,17 @@ $(function(){
 		return false;
 	});
 
+	/* Anchor */
+	/* ---------------------------------------------- */
+
+	$('a.anchor').bind('click.smoothscroll',function () {
+		var target = $(this).attr('href'),
+			bl_top = $(target).offset().top,
+			offset = $(this).data('offset') !== '' ? $(this).data('offset') : 0;
+		$('body,html').animate({scrollTop: bl_top + offset}, 900);
+		return false;
+	});
+
 
 	/* Popup */
 	/* ---------------------------------------------- */
